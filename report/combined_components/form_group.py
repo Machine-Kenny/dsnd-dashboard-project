a7fa217d32c4de0776de94e08fc5318367baf1c2
machine_kenny <adefehintikenny@gmail.com>
2025-01-24 22:04:51 +0100

    automating linting 3i

--- report/combined_components/form_group.py
+++ report/combined_components/form_group.py
@@ -1,8 +1,9 @@
-from .combined_component import CombinedComponent
 from fasthtml.common import Button, Form, Group
 
-class FormGroup(CombinedComponent):
+from .combined_component import CombinedComponent
 
+
+class FormGroup(CombinedComponent):
     id = ""
     action = ""
     method = ""
@@ -12,17 +13,14 @@ class FormGroup(CombinedComponent):
     def call_children(self, userid, model):
         children = super().call_children(userid, model)
         children.append(Button(self.button_label))
-
         return children
 
     def outer_div(self, children, div_args):
-
         return Form(Group(*children), **div_args)
-    
-    def div_args(self, userid, model):
 
+    def div_args(self, userid, model):
         return {
-            'id': self.id,
-            'action': self.action,
-            'method': self.method,
-            }
+            "id": self.id,
+            "action": self.action,
+            "method": self.method,
+        }
