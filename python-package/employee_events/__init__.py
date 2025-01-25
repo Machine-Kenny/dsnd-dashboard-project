efc3a6acfa0698607f7d197c09a79771e18e64ae
machine_kenny <adefehintikenny@gmail.com>
2025-01-25 04:21:51 +0100

    15th

--- python-package/employee_events/__init__.py
+++ python-package/employee_events/__init__.py
@@ -1,1 +1,5 @@
-from .sql_execution import *  # noqa: F403
+# from .sql_execution import *  # noqa: F403
+from .employee import Employee  # noqa: F403
+from .team import Team  # noqa: F403
+
+__all__ = ["Employee", "Team"]
